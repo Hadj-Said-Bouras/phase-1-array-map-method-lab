5ebafa7d1f79bdbd534ee710baebee18f5c421be
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-08-07 05:40:26 +0100

    Done with the assignment

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = tutorials.map(sentence => {
+  const titleCased = sentence.charAt(0).toUpperCase() + sentence.slice(1);
+  return titleCased;
+})
+console.log(titleCased)
